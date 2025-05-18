f42bbb328822cec3fcaaf23d019c9b109634221a
Ajaymanik <125759020+Ajaymanik@users.noreply.github.com>
2025-05-18 13:03:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
+let totalCost = 0;
+
+  while (arr.length > 1) {
+    arr.sort((a, b) => a - b); // sort in ascending order
+    let first = arr.shift(); // smallest rope
+    let second = arr.shift(); // second smallest
+    let cost = first + second;
+    totalCost += cost;
+    arr.push(cost); // insert the combined rope back
+  }
+
+  return totalCost;
   
 }
 
